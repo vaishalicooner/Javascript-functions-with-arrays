95eebf7f70d06f7d908d9af5066837f60bb95117
Hackbright Student <no-reply@hackbrightacademy.com>
2018-09-27 13:06:19 -0700

    done

--- candy.js
+++ candy.js
@@ -13,3 +13,22 @@ else {
  }
  }
 candyColor('purple', candy);
+
+
+function swap(arry) {
+... let x = 0;
+... let y = 0;
+... let temp = null;
+... for (x = arry.length- 1; x> 0;  x -= 1) {
+..... y = Math.floor(Math.random() * (x + 1));
+..... temp = arry[x];
+..... arry[x] = arry[y];
+..... arry[y] = temp;
+..... }
+... }
+undefined
+> console.log(swap(arry));
+undefined
+undefined
+> console.log(swap(arry));
+undefined
